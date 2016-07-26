c2b5eb2ec730f9627977dd118d2f1e5737ed8758
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2016-07-26 12:03:38 +0100

    Add status text

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Creating services..."
 cf create-service cloudantNoSQLDB Shared myMicroservicesCloudant
 
 cf create-service service_discovery free myServiceDiscovery
 
+echo "Pushing applications..."
 cf push
