c36647c6a236daf8e5e9181884aaaefe59ab6dde
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2016-07-26 12:02:26 +0100

    remove obsolete services

--- deploy_all.sh
+++ deploy_all.sh
@@ -4,8 +4,4 @@ cf create-service cloudantNoSQLDB Shared myMicroservicesCloudant
 
 cf create-service service_discovery free myServiceDiscovery
 
-cf create-service mqlight standard mqLightService
-
-cf create-service sqldb sqldb_free myMicroservicesSQL
-
 cf push
