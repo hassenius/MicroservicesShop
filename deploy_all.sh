17c008ed58565af1e3a253f1944ebc19eac16441
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2017-06-29 08:54:20 +0100

    deploy with UI env

--- deploy_all.sh
+++ deploy_all.sh
@@ -4,5 +4,21 @@ echo "Creating services..."
 cf create-service cloudantNoSQLDB Lite myMicroservicesCloudant
 cf create-service newrelic standard NewRelic
 
-echo "Pushing applications..."
-cf push
+echo "Pushing CatalogAPI Microservice..."
+cf push MicroservicesCatalogAPI
+echo "Pushing OrdersAPI Microservice..."
+cf push MicroservicesOrdersAPI
+
+echo "Getting OrderAPI URL "
+OrderURL=$(cf routes | grep -i MicroservicesOrdersAPI | awk '{OFS="." ; print $2,$3}')
+echo -e "Detected ${OrderURL}"
+echo -e "Getting CatalogAPI URL "
+CatalogURL=$(cf routes | grep -i MicroservicesCatalogAPI | awk '{OFS="." ; print $2,$3}')
+echo -e "Detected ${CatalogURL} "
+echo -e "Pushing MicroservicesUI"
+cf push MicroservicesUI --no-start
+echo "Setting environment variables for backend mircroservices"
+cf set-env MicroservicesUI OrderURL ${OrderURL}
+cf set-env MicroservicesUI CatalogURL ${CatalogURL}
+echo "Starting application"
+cf start MicroservicesUI
